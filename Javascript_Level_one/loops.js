bd287db9f2416dc8f8a77e764849e7c8a9165a39
rahulsudhakar10 <62701891+rahulsudhakar10@users.noreply.github.com>
2020-04-15 21:23:21 +0530

    Add files via upload

---
+++ Javascript_Level_one/loops.js
@@ -0,0 +1,9 @@
+var time = prompt("Please enter time in hours?");
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+console.log(greeting);
---
+++ Javascript_Level_one/whileloop.js
@@ -0,0 +1,8 @@
+
+var i = prompt("Please enter i value?");
+var text;
+while (i < 10) {
+ text += "The number is " + i;
+  i++;
+  console.log(text);
+}
